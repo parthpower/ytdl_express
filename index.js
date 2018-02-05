05bb8064f42a3c5523b26ebef2c52154fcdb6368
Parth Parikh <parthpower@gmail.com>
2018-02-05 05:13:08 -0800

    init commit 2

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ app.get('/', (req,res)=>{
 		res.send('Oops!');
 	}
 });
+
+app.get('/vid', (req,res)=>{
+	try{
+		let url = req.query.url;
+		if(ytdl.validateURL(url)){
+			res.setHeader("content-type","video/*");
+			ytdl(url)
+				.pipe(res);
+		}else{
+			res.send("Oops!");
+		}
+	}catch (err){
+		res.send('Oops!');
+	}
+});
+
 app.listen(process.env.PORT|5000,()=>{
 	console.log("Listening to port",process.env.PORT|5000);
 });
