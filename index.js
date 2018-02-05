31401d5e68320ac58475c9711e53b8abcfd5e924
Parth Parikh <parthpower@gmail.com>
2018-02-05 05:09:36 -0800

    init commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+
+const app = express();
+
+app.get('/', (req,res)=>{
+	try{
+		let url = req.query.url;
+		if(ytdl.validateURL(url)){
+			res.setHeader("content-type","audio/aac");
+			ytdl(url, {quality: "highestaudio", filter: "audioonly"})
+				.pipe(res);
+		}else{
+			res.send("Oops!");
+		}
+	}catch (err){
+		res.send('Oops!');
+	}
+});
+app.listen(process.env.PORT|5000,()=>{
+	console.log("Listening to port",process.env.PORT|5000);
+});
