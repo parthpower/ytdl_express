b9ab4bf479ce4ee11496d9da9cf3cca0747c14e7
Parth Parikh <parthpower@gmail.com>
2018-03-22 16:28:51 -0700

    Added search and iamfeeling lucky

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const ytdl = require('ytdl-core');
-var yts = require('youtube-scrape');
+const yts = require('youtube-scrape/scraper').getYouTubeResults;
 
 const app = express();
 
@@ -35,17 +35,42 @@ app.get('/vid', (req,res)=>{
 });
 
 
-app.get('/search',(req,resp)=>{
+app.get('/search',(req,res)=>{
 	try{
 		let query = req.query.q;
 		yts(decodeURI(query)).then((data)=>{
-			resp.send(data);
+			res.send(data);
 		}, (error)=>{
-			resp.send('Oops!');
+			res.send('Oops!');
 		})
 	}catch(err){
 		res.send('Oops!');
 	}
+})
+
+app.get('/iamfeelinglucky',(req,res)=>{
+	try{
+		var query = req.query.q;
+	}catch(err){
+		var query = 'linkin park numb';
+	}
+	yts(query).then((r)=>{
+		try{
+			var youtube_url = r.results[0].video.url;
+		}catch(err){
+			var youtube_url = "https://www.youtube.com/watch?v=q6EoRBvdVPQ";
+		}
+		if(ytdl.validateURL(youtube_url)){
+			res.setHeader("content-type","audio/aac");
+			ytdl(youtube_url, {quality: "highestaudio", filter: "audioonly"})
+				.pipe(res);
+		}else{
+			res.send("Oops!");
+		}
+
+	}).catch(err=>{
+		res.send("Oops!");
+	});
 })
 
 app.listen(process.env.PORT||5000,()=>{
